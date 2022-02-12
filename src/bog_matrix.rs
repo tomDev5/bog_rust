23d4c16bdf5955b89c56b4a520fa2ee330bcb67b
tomDev5 <tom@lubin.ws>
2022-02-12 21:13:42 +0200

    ugly recursive solution works

--- src/bog_matrix.rs
+++ src/bog_matrix.rs
@@ -1,38 +1,87 @@
-struct BogMatrixStruct<T: Copy> {
-    matrix: Vec<T>,
-    num_rows: usize,
-    num_columns: usize,
-}
-
-impl<T: Copy> BogMatrixStruct<T> {
-    fn at(&self, i: usize, j: usize) -> T {
-        assert_eq!(self.num_rows < i, true);
-        assert_eq!(self.num_columns < j, true);
-        let result = self.matrix[(i * self.num_columns) + j];
-        result
-    }
-}
+mod matrix;
+use matrix::*;
 
 pub trait BogMatrix {
-    fn contains(&self, check_word: &String) -> bool;
+    fn contains(&self, check_word: &str) -> bool;
     fn print(&self);
 }
 
-impl<T: Copy + std::fmt::Display> BogMatrix for BogMatrixStruct<T> {
-    fn contains(&self, check_word: &String) -> bool {
-        // insert word finding algorithm
-        true
+fn find_word_recursive(matrix: &MatrixStruct<char>, word: &str, current: (usize, usize)) -> bool {
+    if word.len() == 0 {
+        return true;
+    }
+    if *matrix.at(current) != word.chars().next().unwrap() {
+        return false;
+    }
+    let mut result: bool = false;
+    if current.0 > 0 {
+        result = result || find_word_recursive(matrix, &word[1..], (current.0 - 1, current.1));
+        if current.1 > 0 {
+            result =
+                result || find_word_recursive(matrix, &word[1..], (current.0 - 1, current.1 - 1))
+        }
+        if current.1 < matrix.num_columns - 1 {
+            result =
+                result || find_word_recursive(matrix, &word[1..], (current.0 - 1, current.1 + 1))
+        }
+    }
+
+    if current.0 < matrix.num_rows - 1 {
+        result = result || find_word_recursive(matrix, &word[1..], (current.0 + 1, current.1));
+        if current.1 > 0 {
+            result =
+                result || find_word_recursive(matrix, &word[1..], (current.0 + 1, current.1 - 1))
+        }
+        if current.1 < matrix.num_columns - 1 {
+            result =
+                result || find_word_recursive(matrix, &word[1..], (current.0 + 1, current.1 + 1))
+        }
+    }
+
+    if current.1 > 0 {
+        result = result || find_word_recursive(matrix, &word[1..], (current.0, current.1 - 1));
+        if current.0 > 0 {
+            result =
+                result || find_word_recursive(matrix, &word[1..], (current.0 - 1, current.1 - 1));
+        }
+        if current.0 < matrix.num_rows - 1 {
+            result =
+                result || find_word_recursive(matrix, &word[1..], (current.0 + 1, current.1 - 1));
+        }
+    }
+
+    if current.1 < matrix.num_columns - 1 {
+        result = result || find_word_recursive(matrix, &word[1..], (current.0, current.1 + 1));
+        if current.0 > 0 {
+            result =
+                result || find_word_recursive(matrix, &word[1..], (current.0 - 1, current.1 + 1));
+        }
+        if current.0 < matrix.num_rows - 1 {
+            result =
+                result || find_word_recursive(matrix, &word[1..], (current.0 + 1, current.1 + 1));
+        }
+    }
+    result
+}
+
+impl BogMatrix for MatrixStruct<char> {
+    fn contains(&self, word: &str) -> bool {
+        // word finding algorithm:
+        // find first letter
+        // run recursive algorithm for every neighbor with check_word[1..]
+        // return || of all runs of the algorithm
+        for i in 0..self.num_columns {
+            for j in 0..self.num_rows {
+                if find_word_recursive(self, word, (i, j)) {
+                    return true;
+                }
+            }
+        }
+        false
     }
 
     fn print(&self) {
-        for i in 0..self.num_rows {
-            let start = i * self.num_columns;
-            let end = start + self.num_columns;
-            for j in start..end {
-                print!("{} ", self.matrix[j]);
-            }
-            println!("\n");
-        }
+        self.print();
     }
 }
 
@@ -40,7 +89,7 @@ pub fn bog_matrix_create(num_rows: usize, num_columns: usize) -> impl BogMatrix 
     use rand::prelude::*;
     let mut rng = rand::thread_rng();
 
-    let mut mat = BogMatrixStruct::<char> {
+    let mut mat = MatrixStruct::<char> {
         matrix: Vec::with_capacity(num_rows * num_columns),
         num_rows: num_rows,
         num_columns: num_columns,
@@ -52,19 +101,3 @@ pub fn bog_matrix_create(num_rows: usize, num_columns: usize) -> impl BogMatrix 
     }
     mat
 }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn word_set_add_contains_test() {
-//         let mut word_set = bog_wordset_create();
-//         word_set.add_word(String::from("Word"));
-//         word_set.add_word(String::from("Another"));
-//         word_set.add_word(String::from("Third"));
-//         assert_eq!(word_set.contains(&String::from("Word")), true);
-//         assert_eq!(word_set.contains(&String::from("Another")), true);
-//         assert_eq!(word_set.contains(&String::from("Third")), true);
-//     }
-// }
